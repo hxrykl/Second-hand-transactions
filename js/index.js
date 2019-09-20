2e947b783c31fe5d172f4cca932a52ca424b579b
hxrykl <1574551580@qq.com>
2019-09-20 08:44:11 +0800

    第二次提交，基本完成所有子页面基本功能

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+
+//根地址
+var baseUrl = "http://134.175.154.93:6677";
+var baseUrl2 = "http://134.175.100.63:6677";
+//页面结构加载完时自行加载
 $(function(){
 
 	//为导航添加事件
@@ -12,7 +17,11 @@ $(function(){
 		$("div.s-menu > div > i").remove();
 		//添加图标
 		$(this).append('<i>></i>');
-
+		//获取到菜单名
+		var menun = $(this).children('span').text();
+		//直接写入面包屑元素里
+		$('.j-menun').text(menun);
+		
 		//获取到的html插入匹配的元素中
 		$("#wrapper").load(url);
 	});
