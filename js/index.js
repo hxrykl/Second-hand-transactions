2f7544ebf21cd2c90a297b3f786f5674a3803171
hxrykl <1574551580@qq.com>
2019-09-21 15:53:44 +0800

    与后台交互功能实现，页面丰富有待完善

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 
 //根地址
-var baseUrl = "http://134.175.154.93:6677";
-var baseUrl2 = "http://134.175.100.63:6677";
+
+var baseUrl = "http://134.175.100.63:6677";
 //页面结构加载完时自行加载
 $(function(){
 
 	//为导航添加事件
 	$("div.s-menu").on("click","div",function(){
-		//获取自定义地址
-		var url = $(this).attr("url");
+
 		//删除属性名
 		$("div.s-menu > div").removeClass("j-selection");
 		//添加属性名
@@ -21,9 +20,12 @@ $(function(){
 		var menun = $(this).children('span').text();
 		//直接写入面包屑元素里
 		$('.j-menun').text(menun);
-		
+		//获取自定义地址
+		var url = $(this).attr("url");
 		//获取到的html插入匹配的元素中
 		$("#wrapper").load(url);
+		
+		
 	});
 
 	//自动点击第一个菜单栏
